8ee2c0c977c0e70d66971df434c1175fd6843cd2
Rodrigo <roh.amelo@gmail.com>
2023-01-18 18:52:03 -0300

    Deploy

---
+++ dasoiuads.bat
@@ -0,0 +1,5 @@
+git init
+git add -A .
+git commit -m "Deploy"
+git remote add origin https://github.com/roodriiigooo/quickp2p
+git push -f origin master:gh-pages
---
+++ precache-manifest.3b5e05ec6696091495b7f484bfbd5e01.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "f287531a23d46d22fc93",
+    "url": "/quickp2p/static/js/runtime~main.75399bbd.js"
+  },
+  {
+    "revision": "bfe0c8f889f61ba16066",
+    "url": "/quickp2p/static/js/main.98e549c0.chunk.js"
+  },
+  {
+    "revision": "684c0bc58092806275b2",
+    "url": "/quickp2p/static/js/2.6218f2d0.chunk.js"
+  },
+  {
+    "revision": "f28a7d016bcfdf03e299b825502b990f",
+    "url": "/quickp2p/index.html"
+  }
+];
