fae771d88430cd962fe288e12072ff1289cc6a2c
Rodrigo <roh.amelo@gmail.com>
2024-10-15 14:33:42 -0300

    host change

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import querystring from "query-string";
 import quickp2p, { SimpleStore } from "quickp2p";
 import PlayScreenEgg from "./gui/PlayScreenEgg";
 
-quickp2p.setStore(new SimpleStore("https://misc.r-labs.io"));
-// quickp2p.setStore(
-// 	new SimpleStore(
-// 		"https://roodriiigooo-token-holder.netlify.app/.netlify/functions/server"
-// 	)
-// );
+// quickp2p.setStore(new SimpleStore("https://misc.r-labs.io"));
+quickp2p.setStore(
+	new SimpleStore(
+		"https://roodriiigooo-token-holder.netlify.app/.netlify/functions/server"
+	)
+);
 // quickp2p.setStore(new SimpleStore("http://localhost:5000"));
 
 export default class App extends PureComponent {
