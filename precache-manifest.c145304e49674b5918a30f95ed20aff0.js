57f94975410f59b98792efe83c68c99e7a55f83a
Rodrigo <roh.amelo@gmail.com>
2025-08-26 15:07:49 -0300

    Deploy

---
+++ precache-manifest.c145304e49674b5918a30f95ed20aff0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "826956572aedf64384e1",
+    "url": "/static/css/main.7be87de5.chunk.css"
+  },
+  {
+    "revision": "826956572aedf64384e1",
+    "url": "/static/js/main.023be7f8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "25de19728cecbe658fb5",
+    "url": "/static/css/2.ed861f9f.chunk.css"
+  },
+  {
+    "revision": "25de19728cecbe658fb5",
+    "url": "/static/js/2.50c1f191.chunk.js"
+  },
+  {
+    "revision": "810616fd4635f4a127ecc14be9c730ea",
+    "url": "/static/media/balao.810616fd.svg"
+  },
+  {
+    "revision": "398738697888c915d8fb7c56a84a4a80",
+    "url": "/static/media/rodrigo_profile_picture.39873869.png"
+  },
+  {
+    "revision": "9b04f1821240b1e718f9caa5108c8e54",
+    "url": "/index.html"
+  }
+];
